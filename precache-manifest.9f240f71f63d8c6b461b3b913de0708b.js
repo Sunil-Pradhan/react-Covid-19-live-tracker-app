a1916e3e9f40fd343688568df70708597d67851c
Sunil-Pradhan <pradhan.sunil@outlook.com>
2020-07-12 21:57:07 +0530

    Updates

---
+++ precache-manifest.9f240f71f63d8c6b461b3b913de0708b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b5bd4a7085af6f3ecff857ac793c21b",
+    "url": "/react-Covid-19-live-tracker-app/index.html"
+  },
+  {
+    "revision": "53844d2c4c6329115536",
+    "url": "/react-Covid-19-live-tracker-app/static/css/main.c763a8fb.chunk.css"
+  },
+  {
+    "revision": "968dbcefd08919916b73",
+    "url": "/react-Covid-19-live-tracker-app/static/js/2.c85a6cfd.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/react-Covid-19-live-tracker-app/static/js/2.c85a6cfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53844d2c4c6329115536",
+    "url": "/react-Covid-19-live-tracker-app/static/js/main.0e3abef5.chunk.js"
+  },
+  {
+    "revision": "5dd932fa234839eacae4",
+    "url": "/react-Covid-19-live-tracker-app/static/js/runtime-main.353b0d1d.js"
+  },
+  {
+    "revision": "81582d0edb2348c039f8d1372f784e89",
+    "url": "/react-Covid-19-live-tracker-app/static/media/covid.81582d0e.png"
+  }
+]);
