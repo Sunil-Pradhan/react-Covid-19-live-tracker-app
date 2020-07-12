e481798832bb5f11c850c3feb0faa66b2cdb31e6
Sunil-Pradhan <pradhan.sunil@outlook.com>
2020-07-12 21:52:27 +0530

    URL updated

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+import { Cards, Chart, CountryPicker } from './components';
+import styles from './App.module.css';
+import { fetchData } from './api';
+import coronaImage from './img/covid.png';
+class App extends Component {
+  state = {
+    data: {},
+    country: '',
+  };
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    this.setState({ data: fetchedData });
+  }
+
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+    this.setState({ data: fetchedData, country: country });
+  };
+  render() {
+    const { data, country } = this.state;
+    return (
+      <div className={styles.container}>
+        <img src={coronaImage} className={styles.image} />
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
+      </div>
+    );
+  }
+}
+
+export default App;
